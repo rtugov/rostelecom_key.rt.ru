fbb8a94c8100a297a3da44c037ede604cd74110c
rtugov <rtugov@novakidschool.com>
2026-05-23 10:06:28 +0300

    Fix state

--- get_code.py
+++ get_code.py
@@ -30,68 +30,70 @@ with sync_playwright() as p:
     for file in state_files:
         if not file.endswith(".json"):
             continue
+        try:
+            state_path = os.path.join(STATE_DIR, file)
 
-        state_path = os.path.join(STATE_DIR, file)
+            context = browser.new_context(storage_state=state_path)
+            page = context.new_page()
 
-        context = browser.new_context(storage_state=state_path)
-        page = context.new_page()
+            if args.new_code:
+                page.goto("https://key.rt.ru/main/pwa/dashboard")
+                expect(page.get_by_text("Moй дом")).to_be_visible(timeout=300_000)
+                # input(f"Залогинься и нажми Enter...")
 
-        if args.new_code:
-            page.goto("https://key.rt.ru/main/pwa/dashboard")
-            expect(page.get_by_text("Moй дом")).to_be_visible(timeout=300_000)
-            # input(f"Залогинься и нажми Enter...")
-
-            # Wait for it
-            page.wait_for_timeout(3000)
-            # Look for any existing code's delete button
-            delete_buttons = page.locator('button svg path[fill="#70F"]').count()
-            # print(f"Count - > {delete_buttons}")
-            # input(f"Залогинься и нажми Enter...")
-            
-            if delete_buttons > 0:
-                # Click the first delete button
-                page.locator('button svg path[fill="#70F"]').first.click()
-                
-                # Wait for popup
-                page.wait_for_selector('text=Вы действительно хотите удалить временный код?')
-
-                # Wait for it 
+                # Wait for it
                 page.wait_for_timeout(3000)
+                # Look for any existing code's delete button
+                delete_buttons = page.locator('button svg path[fill="#70F"]').count()
+                # print(f"Count - > {delete_buttons}")
+                # input(f"Залогинься и нажми Enter...")
                 
-                # Confirm deletion
-                page.click('button:has-text("Удалить")', force=True)
+                if delete_buttons > 0:
+                    # Click the first delete button
+                    page.locator('button svg path[fill="#70F"]').first.click()
+                    
+                    # Wait for popup
+                    page.wait_for_selector('text=Вы действительно хотите удалить временный код?')
 
-                # Wait for it to disappear
+                    # Wait for it 
+                    page.wait_for_timeout(3000)
+                    
+                    # Confirm deletion
+                    page.click('button:has-text("Удалить")', force=True)
+
+                    # Wait for it to disappear
+                    page.wait_for_timeout(3000)
+                    # print("Deleted existing code")
+                    page.reload()
+                # input(f"Залогинься и нажми Enter...")
+                # Now create new code
+                page.click('button:has-text("Создать код для домофонов")')
+                # print("Created new code")
+                # Refresh to see updated list
                 page.wait_for_timeout(3000)
-                # print("Deleted existing code")
                 page.reload()
+
+            page.goto("https://key.rt.ru/main/pwa/dashboard")
+            locator = page.locator('[data-testid*="intercom-device-intercode"] p.MuiTypography-mediumBody1')
+            locator.wait_for()
+
+            code = locator.inner_text()
+
+            page.goto("https://key.rt.ru/main/pwa/profile/details")
+            locator = page.locator("p.MuiTypography-root.MuiTypography-regularBody3").nth(0)
+
+            locator.wait_for()
+
+            address = locator.inner_text()
+
+            phone = file.replace(".json", "")
+
+            print(f"+{phone} {address} Код: {code}")
+
             # input(f"Залогинься и нажми Enter...")
-            # Now create new code
-            page.click('button:has-text("Создать код для домофонов")')
-            # print("Created new code")
-            # Refresh to see updated list
-            page.wait_for_timeout(3000)
-            page.reload()
 
-        page.goto("https://key.rt.ru/main/pwa/dashboard")
-        locator = page.locator('[data-testid*="intercom-device-intercode"] p.MuiTypography-mediumBody1')
-        locator.wait_for()
-
-        code = locator.inner_text()
-
-        page.goto("https://key.rt.ru/main/pwa/profile/details")
-        locator = page.locator("p.MuiTypography-root.MuiTypography-regularBody3").nth(0)
-
-        locator.wait_for()
-
-        address = locator.inner_text()
-
-        phone = file.replace(".json", "")
-
-        print(f"+{phone} {address} Код: {code}")
-
-        # input(f"Залогинься и нажми Enter...")
-
-        context.close()
+            context.close()
+        except:
+            print(f"+{phone} : Ошибка")
 
     browser.close()
